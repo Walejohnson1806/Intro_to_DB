58d541976933f282c791d8844fa55978d120d514
Wale Johnson <Walejohnson1806@gmail.com>
2025-02-01 22:21:10 +0100

    Write a script that creates all the tables below in alx_book_store in your MySQL server.

---
+++ task_2.sql
@@ -0,0 +1,35 @@
+--CREATE BOOK TABLE
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT NOT NULL,
+    price DOUBLE NOT NULL,
+    publication_date DATE NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+--CREATE AUTHOR TABLE
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+--CREATE CUSTOMER TABLE
+CREATE TABLE Customers(
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT NOT NULL
+) --CREATE ORDER TABLE
+CREATE TABLE Orders(
+    order_id INT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+) --CREATE ORDER DETAILS TABLE
+CREATE TABLE Order_Details(
+    order_detail_id INT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+)
